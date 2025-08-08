e3243eeeb17c0903ea82be9bf8417a6b0a899dfc
Diego Marczal <dmarczal@gmail.com>
2025-08-08 09:21:00 -0300

    Refactor home header and made it responsible

--- test/system/home/home_test.rb
+++ test/system/home/home_test.rb
@@ -7,22 +7,24 @@ class HomeTest < ApplicationSystemTestCase
     visit root_url
   end
 
-  should 'show background and logo images' do
-    within 'section#hero' do
-      assert_selector "img[alt='#{I18n.t('home.sections.hero.background_alt')}']"
-      assert_selector "img[alt='#{I18n.t('home.sections.hero.logo_alt')}']"
-
-      assert_selector "img[src*='bg/home']"
+  should 'show header with logo and links' do
+    within 'header#home-header' do
+      assert_selector "img[alt='#{I18n.t('home.header.logo_alt')}']"
       assert_selector "img[src*='logo/farma-gray']"
+    end
+
+    within 'header#home-header nav' do
+      assert_selector 'ul li:nth-child(1)', text: t('home.navigation.about')
+      assert_selector 'ul li:nth-child(2)', text: t('home.navigation.team')
+      assert_selector 'ul li:nth-child(3)', text: t('home.navigation.researches')
+      assert_selector 'ul li:nth-child(4)', text: t('home.navigation.awards')
     end
   end
 
-  should 'show navigation links in the correct order' do
-    within 'section#hero nav' do
-      assert_selector 'a:nth-child(1)', text: t('home.navigation.about')
-      assert_selector 'a:nth-child(2)', text: t('home.navigation.team')
-      assert_selector 'a:nth-child(3)', text: t('home.navigation.researches')
-      assert_selector 'a:nth-child(4)', text: t('home.navigation.awards')
+  should 'show background image' do
+    within 'section#hero' do
+      assert_selector "img[alt='#{I18n.t('home.sections.hero.background_alt')}']"
+      assert_selector "img[src*='bg/home']"
     end
   end
 
