063f2fc01434a52fa7b195c5a019ba91a19fe940
Diego Marczal <marczal@utfpr.edu.br>
2025-08-11 17:56:20 -0300

    Style choose profile page

--- app/controllers/users/auth/passwords_controller.rb
+++ app/controllers/users/auth/passwords_controller.rb
@@ -1,3 +1,7 @@
 class Users::Auth::PasswordsController < Devise::PasswordsController
   layout 'devise/application'
+
+  def after_resetting_password_path_for(_resource)
+    users_choose_profile_path
+  end
 end
